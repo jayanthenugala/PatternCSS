1e2557853d2ec9ed3390de9018b15696609301ec
Praveen Bisht <prvnbist@gmail>
2018-03-12 19:05:24 +0530

    Built basic components & homepage
    1. Webpack Configured
    2. Homepage
    3. Basic SCSS Components
    4. Fonts & Logo

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,24 @@ let path = require('path'),
     ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    entry: './src/app.js',
+    entry: {
+        app: path.join(__dirname,'./src/js/app.js'),
+        style: path.join(__dirname,'./src/js/style.js')
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'app.js'
+        filename: 'js/[name].js'
     },
     module: {
         rules: [{
                 test: /\.scss$/,
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
-                    use: ['css-loader', 'sass-loader'],
-                    publicPath: '/dist',
+                    use: ['css-loader', 'sass-loader']
                 })
             },
             {
-                test: /\.(png|jpg|gif)$/,
+                test: /\.(png|jpg|gif|svg)$/,
                 use: [{
                     loader: 'file-loader',
                     options: {
@@ -28,9 +30,18 @@ module.exports = {
                 }]
             },
             {
+                test: /\.(woff|woff2)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                    }
+                }]
+            },
+            {
                 test: /\.pug(\?.*)?$/,
                 use: ['html-loader', 'pug-html-loader']
-
             }
         ]
     },
@@ -49,6 +60,8 @@ module.exports = {
             },
             template: './src/index.pug',
         }),
-        new ExtractTextPlugin('index.css')
+        new ExtractTextPlugin('css/[name].css', {
+            allChunks: true
+        })
     ]
 }
